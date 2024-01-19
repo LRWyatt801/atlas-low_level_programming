30c6f0b839d8de3c6b6d1012bae881672d610013
Logan Wyatt <logan.wyatt@atlasschool.com>
2024-01-19 12:17:08 -0800

    Working comments 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,16 @@
+/*
+ * File: 4-puts.c
+ * Auth: Logan Wyatt
+ */
+
 #include<stdio.h>
 
-/* main - prints "Programming is like building a multilingual puzzle */
-
+/**
+ * main - prints "Programming is like building a multilingual
+ *                puzzle
+ *
+ * return: always 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
