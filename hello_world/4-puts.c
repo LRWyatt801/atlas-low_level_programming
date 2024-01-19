c2171447646bb4dd7a3374b0527e92e15ad45c16
Logan Wyatt <logan.wyatt@atlasschool.com>
2024-01-18 18:44:09 -0800

    Fix 4.something

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-puts("\"Programming is like building a multilingual puzzle")
+#include<stdio.h>
+
+\* Using puts to print a string of characters*\
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
