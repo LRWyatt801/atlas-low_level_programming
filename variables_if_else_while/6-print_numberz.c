a97dd77fff387cc2f6fd28581f6b875910d0f8d7
Logan Wyatt <logan.wyatt@atlasschool.com>
2024-01-21 13:12:57 -0800

    Add 6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+/*
+ * File: 6-print_numberz.c
+ * Auth: Logan Wyatt
+ */
+
+#include<stdio.h>
+
+/**
+ * main - print [0..9] using putchar
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n > 10; n++)
+		putchar('0' + n);
+
+	putchar('\n');
+
+	return (0);
+}
