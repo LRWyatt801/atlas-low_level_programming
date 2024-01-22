7a7a4a3a7cde7ec873e07e998682f4ba2065b6bf
Logan Wyatt <logan.wyatt@atlasschool.com>
2024-01-21 17:11:08 -0800

    Add 9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+/*
+ * File: 9-print_comb.c
+ * Auth: Logan Wyatt
+ */
+
+#include<stdio.h>
+
+/**
+ * main - print all single digits, followed by a ","
+ *        and new line
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar('0' + n);
+		putchar(",");
+		putchar(" ");
+	}
+
+	putchar('\n');
+
+	return (0);
+}
