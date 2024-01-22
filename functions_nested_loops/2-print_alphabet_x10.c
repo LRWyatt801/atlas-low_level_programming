8e426caff6442c8eb286810585ceddf63007f029
Logan Wyatt <logan.wyatt@atlasschool.com>
2024-01-22 13:01:12 -0800

    Add 2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+/*
+ * File: 2-print_alphabet_x10.c
+ * Auth: Logan Wyatt
+ */
+
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabet, followed by new line,
+ *                  10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');
+	}
+}
