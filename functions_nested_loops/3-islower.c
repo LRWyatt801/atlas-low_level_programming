b49b1921a30a87bf95a7c2fba79e1919ea34c221
Logan Wyatt <logan.wyatt@atlasschool.com>
2024-01-22 14:47:23 -0800

    Add 3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+/*
+ * File: 3-islower.c
+ * Auth: Logan Wyatt
+ */
+
+#include "main.h"
+
+/**
+ * int_islower - prints whether or not an int is lowercase
+ *
+ * Return: int
+ */
+
+int _islower(int c)
+{
+	if (c = ['a..z'])
+		_putchar(1 + '0');
+	else
+		_putchar(0 + '0');
+
+	return(int);
+}
