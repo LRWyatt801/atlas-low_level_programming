069782acc3e5656a2b42d779f0f4c9456d189e7f
Logan Wyatt <logan.wyatt@atlasschool.com>
2024-01-22 18:25:23 -0800

    Fix 8.6

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -8,7 +8,7 @@
 /**
  * jack_bauer - print every minute from 00:00 to 23:59
  *
- * Return: always 0
+ * Return: ...
  */
 
 void jack_bauer(void)
@@ -17,18 +17,21 @@ void jack_bauer(void)
 
 	for (a = 0; a <= 2; a++)
 	{
-		for (b = 0; b <= 3; b++)
+		for (b = 0; b <= 9; b++)
 		{
-			for (c = 0; c <= 5; c++)
+			if (a <= 2 && b <= 3)
 			{
-				for (d = 0; d <= 9; d++)
+				for (c = 0; c <= 5; c++)
 				{
-					_putchar(a + '0');
-					_putchar(b + '0');
-					_putchar(':');
-					_putchar(c + '0');
-					_putchar(d + '0');
-					_putchar('\n');
+					for (d = 0; d <= 9; d++)
+					{
+						_putchar(a + '0');
+						_putchar(b + '0');
+						_putchar(':');
+						_putchar(c + '0');
+						_putchar(d + '0');
+						_putchar('\n');
+					}
 				}
 			}
 		}
