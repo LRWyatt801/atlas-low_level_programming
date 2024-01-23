af82ba6db1d8f52a1fd231bc2f7ab7a96cdd6b61
Logan Wyatt <logan.wyatt@atlasschool.com>
2024-01-22 18:09:58 -0800

    Add 8-24_hours.c

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,38 @@
+/*
+ * file: 8-24_hours.c
+ * auth: Logan Wyatt
+ */
+
+include "main.h"
+
+/**
+ * jack_bauer - print every minute from 00:00 to 23:59
+ *
+ * Return: always 0
+ */
+
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 2; a++)
+	{
+		for (b = 0; b <= 3; b++)
+		{
+			for (c = 0; c <= 5; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					_putchar(a);
+					_putchar(b);
+					_putchar(':');
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+	return (0);
+}
+
