3f239e10cc07f0dd03927d1ac6ad6c75c72aa7c3
Logan Wyatt <logan.wyatt@atlasschool.com>
2024-01-24 17:12:56 -0800

    Add 0-isupper.c

---
+++ more_functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+Directory: more_functions_nested_loops
+
+0.isupper - Write a function that checks for uppercase characters;	return 1 if upper and 0 otherwise
+
---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+/*
+ * file: main.h
+ * auth: Logan Wyatt
+ *
+ * Header File
+ */
+
+#include<stdio.h>
+
+/*
+ * Prototypes
+ */
+
+int _putchar(char);
+int _isupper(int c);
+
