98618b024877d00bd8bea5987c41edd8701c2787
Logan Wyatt <logan.wyatt@atlasschool.com>
2024-01-25 14:01:29 -0800

    Fix 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -19,12 +19,8 @@ void print_most_numbers(void)
 
 	for (n = 0; n < 10; n++)
 	{
-		if ((n = 2) || (n = 4))
-			n++;
-		else
-		{
+		if (n != 2 && n != 4)
 			_putchar(n + '0');
-			n++;
 		}
 	}
 	_putchar('\n');
