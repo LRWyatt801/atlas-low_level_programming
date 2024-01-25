b1a78602d25a2ae2784c58c1bbcc09fb6478838b
Logan Wyatt <logan.wyatt@atlasschool.com>
2024-01-24 17:51:56 -0800

    Add 4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+/*
+ * file: 4-print_most_numbers.c
+ * auth: Logan Wyatt
+ */
+
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers 0-9
+ *			except 2 and 4
+ *			followed by new line
+ *
+ * Return: N/A
+ */
+
+void print_most_numbers(void)
+{
+	int n = 0;
+
+	while (n < 2, n > 2, n < 4, n > 4, n < 10)
+	{
+		_putchar(n + '0');
+		n++;
+	}
+	_putchar('\n');
+}
--- more_functions_nested_loops/README.md
+++ more_functions_nested_loops/README.md
@@ -7,4 +7,6 @@ Directory: more_functions_nested_loops
 2-mul.c - Write a funtion that multiplies two inte
 3-print_numbers.c - Write a function that prints the numbers 0-9
 		    followed by a line
+4-print_most_numbers.c - Write a function that prints the number 0-9
+			 followed by a new line
 
