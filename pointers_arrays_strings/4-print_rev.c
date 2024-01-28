20a65efb80589b7ed68702ae778c78edefb165ca
Logan Wyatt <logan.wyatt@atlasschool.com>
2024-01-27 18:45:44 -0800

    4.3 fix betty error

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,7 +6,7 @@
 #include "main.h"
 
 /**
- * print_rev = prints a string in reverse followed by new line
+ * print_rev - prints a string in reverse followed by new line
  * @s: string to be printed
  *
  * Return: n/a
@@ -20,7 +20,7 @@ void print_rev(char *s)
 
 	while (s[i] != '\0')
 		i++;
-	
+
 	len = i;
 
 	for (n = len - 1; n >= 0; n--)
