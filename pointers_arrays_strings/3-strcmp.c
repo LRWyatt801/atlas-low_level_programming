552320a7f5cc204049fdf66cb67d774f4d67ea66
Logan Wyatt <logan.wyatt@atlasschool.com>
2024-02-03 08:50:02 -0800

    Fix betty style error

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,8 +11,8 @@
  * @s2: string to compare
  *
  * Return: 0 if s1 == s2
- * 	   - value if s1 < s2
- * 	   + value if s1 > s2
+ *	   - value if s1 < s2
+ *	   + value if s1 > s2
  */
 
 int _strcmp(char *s1, char *s2)
@@ -20,5 +20,5 @@ int _strcmp(char *s1, char *s2)
 	for ( ; *s1 == *s2; s1++, s2++)
 		if (*s1 == '\0')
 			return (0);
-	return *s1 - *s2;
+	return (*s1 - *s2);
 }
