78dd7d5bcaa64d6faa9166f12beb46398907bed3
Logan Wyatt <logan.wyatt@atlasschool.com>
2024-02-03 14:55:32 -0800

    Fix ASCII scaling

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -16,10 +16,10 @@ char *string_toupper(char *str)
 {
 	int i;
 
-	for (i = 0; *str != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (*str >= 97 && *str <= 122)
-			*str = *str - 38;
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] = str[i] - 32;
 	}
 	return (str);
 }
