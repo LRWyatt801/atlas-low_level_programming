2c88a14a8b45a39770f35bcf85e8a1a2f5de8ff8
Logan Wyatt <logan.wyatt@atlasschool.com>
2024-02-08 13:15:54 -0800

    Add 1-print_rev_recursion.c

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,26 @@
+/*
+ * file: 1-print_rev-recursion.c
+ * auth: Logan Wyatt
+ */
+
+#include "main.h"
+
+/**
+ * _print_rev_recursion(char *s) - prints string in reverse
+ * @s: string to be printed
+ *
+ * Return: n/a
+ */
+
+void _print_rev_recursion(char *s)
+{
+	int i = 0;
+	char tmp = s[i];
+
+	if (*s != '\0')
+	{
+		tmp = s[i];
+		_print_rev_recursion(s + 1);
+		_putchar(tmp);
+	}
+}
--- recursion/README.md
+++ recursion/README.md
@@ -1,4 +1,5 @@
 This directory contains files that I have worked through to learn recursion
 
 0-puts_recursion.c - Write a function that prints a string, followed by a new line.
+1-print_rev_recursion.c - Write a function that prins a string in revers, followed by a new line.
 
