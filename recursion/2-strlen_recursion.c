5e9f0e93c41ccfba2ab5517aefb7033ff5337616
Logan Wyatt <logan.wyatt@atlasschool.com>
2024-02-08 14:36:57 -0800

    Fix 2.1 betty style/doc error

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -6,7 +6,7 @@
 #include "main.h"
 
 /**
- * _strlen_recursion.c - prints the length of a string
+ * _strlen_recursion - prints the length of a string
  * @s: string
  *
  * Return: int length of string
@@ -17,5 +17,5 @@ int _strlen_recursion(char *s)
 	if (*s == '\0')
 		return (0);
 	else
-		return 1 + _strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 }
