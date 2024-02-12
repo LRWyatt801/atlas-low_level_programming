149bba47f5924180db914fadf35ef50aa73e8e0d
Logan Wyatt <logan.wyatt@atlasschool.com>
2024-02-12 15:25:11 -0800

    Add 4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,35 @@
+/*
+ * file: 4-add.c
+ * auth: Logan Wyatt
+ */
+
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * main - adds up positive int inputs
+ * @argc: number of inputs
+ * @argv: array of inputs
+ *
+ * Return: 0 on success and 1 on error
+ */
+
+int main(int argc, char **argv)
+{
+	int i, j, sum;
+
+	for(i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
--- argc_argv/README.md
+++ argc_argv/README.md
@@ -4,4 +4,4 @@ Programs used to teach the use of argc and argv
 1-args.c - Write a program that prints the number of arguments passed into it, followed by new line.
 2-args.c - write a program that prints all arguments it recieves.
 3-mul.c - Write a program that multiplies two numbers.
-
+4-add.c - Write a program that adds positive numbers.
