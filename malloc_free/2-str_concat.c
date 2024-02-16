70a964e9089d59c30e7605f0a7c063322bd7fb7f
Logan Wyatt <logan.wyatt@atlasschool.com>
2024-02-15 20:16:29 -0800

    Fix 2.1 NULL strings error

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -24,12 +24,8 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; s1[i] != '\0'; i++)
 		;
-	if (s1 == NULL)
-		s1 = 0;
 	for (j = 0; s2[j] != '\0'; j++)
 		;
-	if (s2 == NULL)
-		s2 = 0;
 
 	len = i + j + 1;
 	leni = i;
