0e894ba7d6115265e953bf99818b01794e630480
Logan Wyatt <logan.wyatt@atlasschool.com>
2024-02-15 20:10:36 -0800

    Add 2-str_concat.c

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,58 @@
+/*
+ * file: 2-str_concat.c
+ * auth: Logan Wyatt
+ */
+
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * str_concat - concatenate two strings
+ * @s1: string
+ * @s2: string
+ *
+ * Return: pointer to s1 + s2 or NULL on fail
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *strcat;
+	int i, j; /* i for s1, j for s2 */
+	size_t len; /* lenth of strcat */
+	int x = 0, leni, lenj; /* counter for cat */
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	if (s1 == NULL)
+		s1 = 0;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+	if (s2 == NULL)
+		s2 = 0;
+	len = i + j + 1;
+	leni = i;
+	lenj = j;
+
+	strcat = malloc(sizeof(char) * len);
+	if (strcat == NULL)
+		return (NULL);
+
+	i = 0;
+	while (x < leni)
+	{
+		strcat[x] = s1[i];
+		x++;
+		i++;
+	}
+	j = 0;
+	while (x <= leni + lenj)
+	{
+		strcat[x] = s2[j];
+		x++;
+		j++;
+	}
+	strcat[x] = '\0';
+
+	return (strcat);
+}
--- malloc_free/README.md
+++ malloc_free/README.md
@@ -2,4 +2,5 @@ Learning to use malloc and free with these programs
 
 0-create_array.c - Write a functino that creates an array of chars, and initialize it with a specific char
 1-strdup.c - Write a function that returns a pointer to a newly allocated space in memory, which contains a copy of the given string.
+2-str_concat.c - Write a function that concatenates two strings.
 
