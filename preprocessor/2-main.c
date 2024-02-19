883b52998529f147ad8e439e7414bc359831a72e
Logan Wyatt <logan.wyatt@atlasschool.com>
2024-02-19 12:29:20 -0800

    Add 2-main.c

---
+++ preprocessor/2-main.c
@@ -0,0 +1,17 @@
+/*
+ * file: 2-main.c
+ * auth: Logan Wyatt
+ */
+
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the file it was compiled from
+ *
+ * Return: n/a
+ */
+
+void main(void)
+{
+	printf("%s\n", __FILE__);
+}
