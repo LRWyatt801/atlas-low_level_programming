1c1caee08a60b472f7c1f9eecc842a92407e7e95
Logan Wyatt <logan.wyatt@atlasschool.com>
2024-02-18 18:23:19 -0800

    Add 2-calloc.c

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,34 @@
+/*
+ * file: 2_calloc.c
+ * auth: Logan Wyatt
+ */
+
+#include <stdio.c>
+#include <stdlib.c>
+#include "main.c"
+
+/**
+ * _calloc - allocates memory for an array using malloc
+ * @nmemb: number of elements for array
+ * @size: size of elements in bytes
+ *
+ * Return: NULL for failure, nmemb = 0, size = 0
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *arr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	arr = malloc(size * nmemb);
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		arr[i] = 0;
+	
+	return (arr);
+}
--- more_malloc_free/README.md
+++ more_malloc_free/README.md
@@ -2,4 +2,5 @@ Practice with memory allocation
 
 0-malloc_checked.c - Write a function that allocates memory using malloc.
 1-string_nconcat.c - Write a function that concatenates two strings.
+2-calloc.c - Write a function that allocates memory for an array, using malloc.
 
