84b0f3348da03d33b7ec5eb443433463806644b0
LRWyatt801 <89785615+LRWyatt801@users.noreply.github.com>
2024-02-20 11:21:51 -0600

    Fix 2.1 Null string error

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -22,6 +22,11 @@ char *str_concat(char *s1, char *s2)
 	size_t len; /* lenth of strcat */
 	int x = 0, leni, lenj; /* counter for cat */
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	
 	for (i = 0; s1[i] != '\0'; i++)
 		;
 	for (j = 0; s2[j] != '\0'; j++)
@@ -30,9 +35,6 @@ char *str_concat(char *s1, char *s2)
 	len = i + j + 1;
 	leni = i;
 	lenj = j;
-
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
 
 	strcat = malloc(sizeof(char) * len);
 	if (strcat == NULL)
