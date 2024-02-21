51ad5bd48c2a6e7b264c677f1bfe04e8343a687d
Logan Wyatt <logan.wyatt@atlasschool.com>
2024-02-21 13:49:38 -0800

    Fix 1.1 compile error

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -13,10 +14,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		exit (NULL);
-
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
