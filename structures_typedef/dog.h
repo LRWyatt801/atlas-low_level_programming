54e1880c88a89fcf542af394067f76f1fc598428
Logan Wyatt <logan.wyatt@atlasschool.com>
2024-02-21 13:22:00 -0800

    Fix 0.2 betty style error

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,15 @@
 #ifndef _DOG_H
 #define _DOG_H
 
-struct dog {
+/**
+ * struct dog - contains info for a dog and owner
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
