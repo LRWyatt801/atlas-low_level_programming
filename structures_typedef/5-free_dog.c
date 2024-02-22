579584660bc6b78eef0138fec538bcadc99a0e27
Logan Wyatt <logan.wyatt@atlasschool.com>
2024-02-21 18:34:11 -0800

    Fix 5.2 account for null dog

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -2,8 +2,18 @@
 #include <stdlib.h>
 #include "dog.h"
 
+/**
+ * free_dog - free dog in memory
+ * @d: dog to be freed
+ *
+ * Return: n/a
+ */
+
 void free_dog(dog_t *d)
 {
+	if (d == NULL)
+		return (NULL);
+
 	free(d->name);
 	free(d->owner);
 	free(d);
