5abd4c30e485e13656a7f8d731b172d8bf95b25c
LRWyatt801 <loganwyatt1995@gmail.com>
2024-03-06 18:05:05 -0800

    Add 0-print_list.c Status:functional

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all elements within a list
+ * @list_t: list given to be printed
+ *
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
