d7fcff2a38582318ebd43b04df0d79c0f949de5e
LRWyatt801 <loganwyatt1995@gmail.com>
2024-03-09 13:11:16 -0800

    Add 0-print_listint.c Status: Functional

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
---
+++ more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This repository is practice to learn about singly linked lists
