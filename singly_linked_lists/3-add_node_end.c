56b1462cb33f578d8f1fd2828777924860b94c9d
LRWyatt801 <loganwyatt1995@gmail.com>
2024-03-09 12:42:36 -0800

    Add 3-add_node_end.c Status: Functional

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node_end - adds a node to the end of a singly list
+ * @head: head of list
+ * @str: name of node
+ *
+ * Return: add of the new element, or NULL on fail
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *tmp;
+	unsigned int len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len])
+		len++;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	if (new->str == NULL);
+	{
+		free(new);
+		return(NULL);
+	}
+
+	new->len = len;
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		tmp = *head;
+		while(tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = new;
+	}
+	return (new);
+}
