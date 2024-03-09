a5b8d403ae1142452ea2a78947e745f94b4ea8e0
LRWyatt801 <loganwyatt1995@gmail.com>
2024-03-09 14:24:25 -0800

    Fixed for head = NULL

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -4,7 +4,7 @@
 #include "lists.h"
 
 /**
- * free_listint - frees list
+ * free_listint2 - frees list
  * @head: beginning of list
  *
  * Return: n/a
@@ -13,6 +13,9 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
+
+	if (head == NULL)
+		return;
 
 	while (*head != NULL)
 	{
