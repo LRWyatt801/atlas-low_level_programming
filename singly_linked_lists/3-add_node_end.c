f0d45251ade9fcc1d6ea293324b8f309ce88c198
LRWyatt801 <loganwyatt1995@gmail.com>
2024-03-09 12:43:27 -0800

    Add 3-add_node_end.c Status: Functional

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -27,7 +27,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 
 	new->str = strdup(str);
-	if (new->str == NULL);
+	if (new->str == NULL)
 	{
 		free(new);
 		return(NULL);
