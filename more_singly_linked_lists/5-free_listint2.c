b56d2ec08e195966b30ad0b1be7cbd7d712e16ce
LRWyatt801 <loganwyatt1995@gmail.com>
2024-03-09 14:17:20 -0800

    Add 5-free_listint2.c

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint - frees list
+ * @head: beginning of list
+ *
+ * Return: n/a
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
+	*head = NULL;
+}
