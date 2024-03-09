4e6f9cbe61240571bedbe2a3afc3d2c9b305e81f
LRWyatt801 <loganwyatt1995@gmail.com>
2024-03-09 13:57:21 -0800

    Testing code

---
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_nodeint_end - adds node at the end of list
+ * @head: beginning of list
+ * @n: new node to add
+ *
+ * Return: address of the new elemnet, or NULL on failure
+ */
+
+listint_t *add_nodeint_add(listint_t **head, const int n)
+{
+	listint_t *new, *tmp;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (!*head)
+		*head = new;
+	else
+	{
+		tmp = *head;
+		while (tmp->next == NULL)
+			tmp = tmp->next;
+		tmp->next = new;
+	}
+	return (new);
+}
