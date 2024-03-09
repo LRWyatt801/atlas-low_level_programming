94e65c4bef0dab320be6aa0764087df588d87890
LRWyatt801 <loganwyatt1995@gmail.com>
2024-03-09 11:27:28 -0800

    Add 2-add_node.c Status: Functional

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,5 +11,34 @@
  * Return: address of the new element, or NULL if it failed
  */
 
-list_t *add_node(list_t **head, const char *str);
+list_t *add_node(list_t **head, const char *str)
 {
+	list_t *new_node;
+	
+	if (head == NULL)
+		return (NULL);
+
+	/* set mem for node */
+
+	new_node = NULL;
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	/* duplicate mem for str in node */
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node->str);
+		return (NULL);
+	}
+
+	/* set str len */
+	new_node->len = strlen(str);
+
+	/* make new_node the head */
+	new_node->next = *head;
+	*head = new_node;
+
+	return (*head);
+}
