15b25d5d7c642677966f68bdf1fab2dcb4706a91
LRWyatt801 <loganwyatt1995@gmail.com>
2024-03-09 13:13:22 -0800

    Made Betty happy

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * print_listint - prints a singly listint
+ * @h: head of list
+ *
+ * Return: number of nodes
+ */
+
 size_t print_listint(const listint_t *h)
 {
 	size_t i = 0;
