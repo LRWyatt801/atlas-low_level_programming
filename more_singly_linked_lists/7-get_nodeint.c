7ac91d310d953dfb394c513cba4e0a7660f71b1d
LRWyatt801 <loganwyatt1995@gmail.com>
2024-03-10 09:52:49 -0700

    Add 7-get_nodeint.c

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - return nth node of linked list
+ * @head: head of list
+ * @index: node to return
+ *
+ * Return: nth node of list, if node does not exist return NULL
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *tmp;
+	unsigned int i;
+
+	tmp = head;
+	for (i = 0; i < index; i++)
+		tmp = tmp->next;
+
+	return (tmp);
+}
