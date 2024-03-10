6cef939ece3bd044fba7399c1e0c13fa5040d468
LRWyatt801 <loganwyatt1995@gmail.com>
2024-03-10 09:58:05 -0700

    Fixed for index greater than list len

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -18,7 +18,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	tmp = head;
 	for (i = 0; i < index; i++)
+	{
+		if (tmp->next == NULL && i < index)
+			return (NULL);
 		tmp = tmp->next;
-
+	}
+	
 	return (tmp);
 }
