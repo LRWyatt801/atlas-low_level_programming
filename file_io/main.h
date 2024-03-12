58652d919909bed88352e830f2468d40622e4df9
LRWyatt801 <loganwyatt1995@gmail.com>
2024-03-11 17:38:02 -0700

    Add system header

--- file_io/main.h
+++ file_io/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+
 /* prototypes */
 
 ssize_t read_textfile(const char *filename, size_t letters);
