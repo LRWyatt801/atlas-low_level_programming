d94437e19e97f738f0ce92c08194f71abbf0458f
LRWyatt801 <loganwyatt1995@gmail.com>
2024-03-11 17:52:24 -0700

    Add 2-append_text_to_file.c

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: string of text
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file, count;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_WRONLY | O_APPEND);
+	if (file == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		count = write(file, text_content, strlen(text_content));
+		if (count == -1)
+		{
+			close(file);
+			return (-1);
+		}
+	}
+	close(file);
+	return (1);
+}
