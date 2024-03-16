79847e22cc1ffd835bbc4adfe25c601ba952c233
LRWyatt801 <loganwyatt1995@gmail.com>
2024-03-16 23:53:30 +0000

    I'm going to bang my head against the wall

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -25,7 +25,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	index = key_index((const unsigned char *)key, ht->size);
 
 	tmp = ht->array[index];
-	if (strcmp(tmp->key, key) == 0) /* update existing node */
+	if (tmp != NULL && strcmp(tmp->key, key) == 0) /* update existing node */
 	{
 		strcpy(ht->array[index]->value, value);
 		return (1);
