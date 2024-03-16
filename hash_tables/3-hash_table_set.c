31f58b3e510ce00a7d2ab1ab6cf12e6f1302827c
LRWyatt801 <loganwyatt1995@gmail.com>
2024-03-16 23:25:52 +0000

    test

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -24,7 +24,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	index = key_index((const unsigned char *)key, ht->size);
 
-	/*if (strcmp(ht->array[index]->key, key) == 0) /*update existing node *
+	/*if (strcmp(ht->array[index]->key, key) == 0) * update existing node *
 	{
 		free(ht->array[index]->value);
 		ht->array[index]->value = newstr;
