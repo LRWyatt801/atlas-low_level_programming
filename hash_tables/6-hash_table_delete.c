08d45c192b931824587af03880c302756431e3a9
LRWyatt801 <loganwyatt1995@gmail.com>
2024-03-17 21:37:03 +0000

    Add 6-hash_table_delete.c

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: table to delete
+ *
+ * Return: n/a
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *tmp, *del;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			tmp = ht->array[i];
+			while (tmp != NULL)
+			{
+				del = tmp;
+				tmp = tmp->next;
+				free(del);
+			}
+		}
+	}
+	free(ht);
+}
