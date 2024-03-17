ad628d88429d2177231a776cc2d020ac913486f1
LRWyatt801 <loganwyatt1995@gmail.com>
2024-03-17 20:42:58 +0000

    Collision fix

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -11,6 +11,7 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
+	hash_node_t *data;
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
@@ -19,8 +20,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (index >= ht->size)
 		return (NULL);
 	
-	if (ht->array[index]->value == NULL)
-		return (NULL);
-	else
-		return(ht->array[index]->value);
+	data = ht->array[index];
+	while (data != NULL && strcmp(data->key, key) != 0)
+		data = data->next;
+	
+	return((data == NULL) ? NULL : data->value);
 }
