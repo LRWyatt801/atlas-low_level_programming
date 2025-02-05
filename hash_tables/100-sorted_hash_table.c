edbc97cf0174142608f99f44abb56844b0833d33
LRWyatt801 <loganwyatt1995@gmail.com>
2025-02-04 21:05:28 -0600

    Complete create function

--- hash_tables/100-sorted_hash_table.c
+++ hash_tables/100-sorted_hash_table.c
@@ -1,5 +1,6 @@
 #include "hash_tables.h"
 
+
 /**
 * shash_table_create - creates a hash table
 * @size: size of table
@@ -9,7 +10,29 @@
 
 shash_table_t *shash_table_create(unsigned long int size)
 {
+	shash_table_t *newHash;
+	unsigned long int i = 0;
 
+	newHash = malloc(sizeof(hash_table_t));
+	if (newHash == NULL)
+		return (NULL);
+
+	size = size_align(size);
+	newHash->size = size;
+	newHash->array = malloc(sizeof(hash_node_t *) * size);
+	if (newHash->array == NULL)
+	{
+		free(newHash);
+		return (NULL);
+	}
+
+	for (i = 0; i <= size; i++)
+		newHash->array[i] = NULL;
+
+	newHash->shead = NULL;
+	newHash->stail = NULL;
+
+	return (newHash);
 }
 
 /**
@@ -76,3 +99,28 @@ void shash_table_delete(shash_table_t *ht)
 
 }
 
+/********** HELPER FUNCS **********/
+
+/**
+* align_size - rounds up to the next power of 2
+* @size: size given to round
+*
+* Return: rounded up to power of 2
+*/
+
+unsigned long int align_size(unsigned long int size)
+{
+	if (size == 0) return 1;  // Edge case: 0 should round up to 1
+
+	size--;  // Decrement to handle exact powers of 2 correctly
+	size |= size >> 1;
+	size |= size >> 2;
+	size |= size >> 4;
+	size |= size >> 8;
+	size |= size >> 16;
+	if (sizeof(size) > 4) /* If 64-bit, continue shifting */
+		size |= size >> 32;
+	size++;  /* Increment to get the next power of 2 */
+
+	return (size);
+}
